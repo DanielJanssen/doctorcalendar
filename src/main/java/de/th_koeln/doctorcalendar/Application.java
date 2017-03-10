711005dc8855025155c8fb373cf94b636014d555
danieljanssen@canda.com <rt57@DEN0251.canda.biz>
2017-03-10 09:55:35 +0100

    add database and default UI

--- src/main/java/de/th_koeln/doctorcalendar/Application.java
+++ src/main/java/de/th_koeln/doctorcalendar/Application.java
@@ -2,8 +2,10 @@ package de.th_koeln.doctorcalendar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,5 +15,12 @@ public class Application {
 	public static void main(String[] args) {
 		LOGGER.info("Starting application");
 		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+
+		};
 	}
 }
---
+++ src/main/java/de/th_koeln/doctorcalendar/gui/DoctorUI.java
@@ -0,0 +1,16 @@
+package de.th_koeln.doctorcalendar.gui;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+@Theme("valo")
+public class DoctorUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest aRequest) {
+	}
+
+}
